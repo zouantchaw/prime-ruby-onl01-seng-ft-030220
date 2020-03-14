270ab616814261640c97c55c46f991e9a22aab51
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-03-14 21:13:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
-# Add  code here!
 def prime?(num)
-  (2..(num - 1)).each do |n|
-    return false if num % n == 0
+  if num < 0
+    return false
+  elsif num > 2
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
   end
   true
 end
